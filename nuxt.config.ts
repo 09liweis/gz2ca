cc3a537f80208600599d7053fe5b0e369736a38a
Sam Li <weisen.li@hotmail.com>
2026-03-14 02:26:18 +0000

    add mongoose

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-mongoose'],
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
-  }
+  },
+  mongoose: {
+    uri: process.env.MONGODB_URI,
+    options: {},
+    modelsDir: 'models',
+    devtools: true,
+  },
 })
