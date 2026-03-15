f41f9605f8255860241eaac08d730328dbd92e5c
Sam Li <weisen.li@hotmail.com>
2026-03-15 16:31:34 +0000

    fix create event button

--- app/pages/events/index.vue
+++ app/pages/events/index.vue
@@ -12,7 +12,6 @@
             v-if="isLoggedIn"
             @click="showCreateModal = true"
             variant="primary"
-            class="bg-white text-blue-600 hover:bg-blue-50 px-8 py-3 text-lg font-semibold shadow-lg"
           >
             <svg class="w-5 h-5 inline-block mr-2 -mt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
