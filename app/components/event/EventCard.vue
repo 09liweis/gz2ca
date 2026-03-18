b497c40e402265078248eed514b774bfe30b2e44
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2026-03-18 18:11:26 +0000

    update event place name

--- app/components/event/EventCard.vue
+++ app/components/event/EventCard.vue
@@ -49,14 +49,14 @@
           </div>
           <span class="font-medium text-gray-700">{{ event.place_id.city }}</span>
         </div>
-        <div v-if="event.place_id?.full_address" class="flex items-center">
+        <div v-if="event.place_id?.name" class="flex items-center">
           <div class="w-8 h-8 bg-gray-100 rounded-lg flex items-center justify-center mr-3">
             <svg class="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
             </svg>
           </div>
-          <span class="text-gray-700 line-clamp-1">{{ event.place_id.full_address }}</span>
+          <span class="text-gray-700 line-clamp-1">{{ event.place_id.name }}</span>
         </div>
       </div>
 
