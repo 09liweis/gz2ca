dbd9cc617ff03215d40ffca51dd996febec1f8bf
Sam Li <weisen.li@hotmail.com>
2026-03-19 15:07:10 +0000

    add vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-mongoose'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-mongoose','@vercel/analytics'],
   runtimeConfig: {
     googleClientId: process.env.GOOGLE_CLIENT_ID,
     public: {
